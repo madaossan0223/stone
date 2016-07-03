b773ad49f68056c320be34182c7d967d661ac290
heshuxiong <heshuxiong@wingtech.com>
2016-07-03 23:21:08 +0800

    Modify Reason: comment for ASTree Product: stone language Comments: Abstract Syntax Tree 的对象类继承结构

--- ast/ASTree.java
+++ ast/ASTree.java
@@ -2,6 +2,26 @@ package stone.ast;
 
 import java.util.Iterator;
 
+/*
+ *               +------------+
+ *               |   ASTree   |
+ *               |------------| <-------------------+
+ *               |child(int)  |                     |
+ *               |children()  |                     |
+ *               +------------+                     |
+ *                     ^                            |
+ *                     |                            |
+ *             +-----------------------+            |
+ *             |                       |            |
+ *          ASTLeaf                  ASTList  <>----+
+ *             ^                       ^
+ *             |                       |
+ *         +---+--------+              |
+ *         |            |              |
+ *    NumberLiteral    Name         BinaryExpr
+ *
+ */
+
 public abstract class ASTree implements Iterable<ASTree> {
     public abstract ASTree child(int i);
     public abstract int numChildren();
